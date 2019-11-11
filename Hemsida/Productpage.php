03f5097fa6e3a6e4d1468fcb474ee5d9af2c08e8
robdan7 <robdan-7@student.ltu.se>
2019-11-11 11:16:41 +0100

    php börjar se fint ut :)

---
+++ Hemsida/Productpage.php
@@ -0,0 +1,177 @@
+<?php
+    /* Källa: https://stackoverflow.com/questions/768431/how-do-i-make-a-redirect-in-php*/
+    function redirect($url, $statusCode = 303) {
+        header('Location: ' . $url, true, $statusCode);
+        die();
+    }
+?>
+<?php
+    $servername = "localhost";
+    $username = "";
+    $password = "";
+    $dbname = "website";
+    $productID = $_GET["ProductNumber"];    // Produktnummer från URL
+
+    // Koppla upp mot servern.
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Kolla uppkopplingen.
+    if ($conn->connect_error) {
+            redirect("404.html");
+    }
+
+    /*
+    * Bra info
+    * https://www.w3schools.com/php/php_superglobals_get.asp
+    */
+
+    /* Den här öppnar 404.html om ett produktnummer inte finns */
+    if ($productID == NULL) {
+        redirect("404.html");
+    }
+
+    $sql_query = "SELECT * FROM Products WHERE ProductNumber='".$productID."'";
+    $query_result = $conn->query($sql_query);
+
+    $product = $query_result->fetch_assoc();
+
+
+    //while($row = $query_result->fetch_assoc()) {
+    //        echo "Produkt: ".$row["ProductName"]." Färg: ".$row["ProductColor"]." Produktnummer: ".$row["ProductNumber"]." Pris: ".$row["ProductPrice"]." I lager: ".$row["InStock"]."<br>";
+    //}
+    //echo $query_result["ProductNumber"];
+    //$conn->close();
+    //echo $query_result["ProductNumber"];
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+
+
+    </head>
+    <body>
+        <div id="container">
+
+            <div id="header">
+
+                <h1>Pencil.in</h1>
+                <h3>Botemedlet mot dåliga pennor</h3>
+            </div>
+
+            <div class="nav">
+
+                <ul>
+
+                    <li>
+                        <a href="Home.html">Hem</a>
+                    </li>
+                    <li>
+                        <a>Bläckpennor</a>
+                        <ul>
+                            <li><a href="Productpage.html">Penna1</a></li>
+                            <li><a href="Productpage.html">Penna2</a></li>
+                            <li><a href="Productpage.html">Penna3</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a>Blyertspennor</a>
+                        <ul>
+                            <li><a href="Productpage.html">Penna1</a></li>
+                            <li><a href="Productpage.html">Penna2</a></li>
+                            <li><a href="Productpage.html">Penna3</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a>Resevoarpennor</a>
+                        <ul>
+                            <li><a href="Productpage.html">Penna1</a></li>
+                            <li><a href="Productpage.html">Penna2</a></li>
+                            <li><a href="Productpage.html">Penna3</a></li>
+                        </ul>
+                    </li>
+
+                    <li>
+                         <a href="User.html">Användarsida</a>
+                    </li>
+                    <div class="shoppingcart">
+                        <li>
+                            <a href="Shoppingcart.html">
+                                <img src="shoppingcart.png" width="50px" height="50px" >
+                            </a>
+
+                        </li>
+                    </div>
+
+                </ul>
+
+
+            </div>
+
+
+            <div id="product">
+                <img src="Penna1.jpg">
+            </div>
+            <table class="responsive-table">
+                <caption><?php echo $product["ProductName"];?></caption>
+                <tbody>
+                    <tr>
+                        <th scope="row">Produktyp</th>
+                        <td><?php echo $product["ProductType"];?></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Färg</th>
+                        <td><?php echo $product["ProductColor"]?></td>
+                    </tr>
+                    <th scope="row"><BR>&nbsp;</BR></th>
+                    <td></td>
+                    <tr>
+                        <th scope="row">Pris</th>
+                        <td><?php echo $product["ProductPrice"]?> kr</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Lagerstatus</th>
+                        <td>
+                            <?php
+                                if ($product["InStock"] == 0) {
+                                    echo "Slut i lager";
+                                } else {
+                                    echo $product["InStock"]. " st";
+                                }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Artikelnummer</th>
+                        <td>0097</td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+            <div class= "buyButton">
+
+                <p> Köp </p>
+
+            </div>
+
+            <div id = "reviewdiv">
+
+                <p>  Lämna en recension!    </p>
+
+
+            </div>
+
+
+
+
+        </div>
+
+    </body>
+
+</html>
+<?php
+    mysql_free_result($query_result);
+?>
--- Hemsida/index.php
+++ Hemsida/index.php
@@ -25,7 +25,7 @@ Just nu går det dessutom bara att använda SELECT på Products.
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-        $sql = "SELECT ProductName FROM Products WHERE ProductType='Bläckpenna'";
+        $sql = "SELECT * FROM Products WHERE ProductNumber='3003'";
         $result = $conn->query($sql);
 
         /*
@@ -36,14 +36,35 @@ Just nu går det dessutom bara att använda SELECT på Products.
 
         if ($result->num_rows > 0) {
             // output data of each row
-            echo "Våra bläckpennor: <br>";
-            while($row = $result->fetch_assoc()) {
-                echo "Produkt: ".$row["ProductName"]." Färg: ".$row["ProductColor"]." Produktnummer: ".$row["ProductNumber"]." Pris: ".$row["ProductPrice"]." I lager: ".$row["InStock"]."<br>";
-            }
-
+            echo "Våra reservoarpennor: <br>";
+            //while($row = $result->fetch_assoc()) {
+            //    echo "Produkt: ".$row["ProductName"]." Färg: ".$row["ProductColor"]." Produktnummer: ".$row["ProductNumber"]." Pris: ".$row["ProductPrice"]." I lager: ".$row["InStock"]."<br>";
+            //    echo "<a href='index.php?ProductNumber=".$row['ProductNumber'],"'>penna</a> <br>";
+            //}
         } else {
             echo "0 results";
         }
+
+        $test = $result->fetch_assoc();
+        echo "hello ".$test["ProductName"];
+        //$productCode = mysql_real_escape_string($_SERVER['QUERY_STRING']);
+
+        /*
+        * Bra info
+        * https://www.w3schools.com/php/php_superglobals_server.asp
+        */
+
+        /* Hämta variablerna som finns i URL:en och stoppa in dom i en lista*/
+        $query = $_SERVER['QUERY_STRING'];
+        parse_str($query,$parse_query);
+        //echo $parse_query["ProductNumber"];
+
+        /* Den här öppnar 404.html om ett produktnummer inte finns */
+        if ($parse_query["ProductNumber"] == NULL) {
+            echo "<meta http-equiv='refresh' content='0;url=404.html'>";
+        }
+
+        mysql_free_result($result[0]);
         $conn->close();
         ?>
     </body>
