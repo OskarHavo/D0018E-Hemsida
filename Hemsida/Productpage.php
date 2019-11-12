86cb02b374907b963a335977ffcbb0430e6d6c2c
OskarHavo <32604330+OskarHavo@users.noreply.github.com>
2019-11-12 18:23:16 +0100

    Javascript för navigeringsbar
    Hittat ett JS och implementerat det så vi slipper ändra header / navbar i varje fil.
    Källan finns i toppen av filen

--- Hemsida/Productpage.php
+++ Hemsida/Productpage.php
@@ -49,68 +49,13 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="style.css">
+        <script src="csi.js"></script>
 
 
     </head>
     <body>
+        <div data-include="navbar.html"></div>
         <div id="container">
-
-            <div id="header">
-
-                <h1>Pencil.in</h1>
-                <h3>Botemedlet mot dåliga pennor</h3>
-            </div>
-
-            <div class="nav">
-
-                <ul>
-
-                    <li>
-                        <a href="Home.html">Hem</a>
-                    </li>
-                    <li>
-                        <a>Bläckpennor</a>
-                        <ul>
-                            <li><a href="Productpage.php?ProductNumber=1001">Penna1</a></li>
-                            <li><a href="Productpage.php?ProductNumber=1002">Penna2</a></li>
-                            <li><a href="Productpage.php?ProductNumber=1003">Penna3</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a>Blyertspennor</a>
-                        <ul>
-                            <li><a href="Productpage.php?ProductNumber=2001">Penna1</a></li>
-                            <li><a href="Productpage.php?ProductNumber=2002">Penna2</a></li>
-                            <li><a href="Productpage.php?ProductNumber=2003">Penna3</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a>Resevoarpennor</a>
-                        <ul>
-                            <li><a href="Productpage.php?ProductNumber=3001">Penna1</a></li>
-                            <li><a href="Productpage.php?ProductNumber=3002">Penna2</a></li>
-                            <li><a href="Productpage.php?ProductNumber=3003">Penna3</a></li>
-                        </ul>
-                    </li>
-
-                    <li>
-                         <a href="User.html">Användarsida</a>
-                    </li>
-                    <div class="shoppingcart">
-                        <li>
-                            <a href="Shoppingcart.html">
-                                <img src="shoppingcart.png" width="50px" height="50px" >
-                            </a>
-
-                        </li>
-
-                    </div>
-
-                </ul>
-
-
-            </div>
-
 
             <div id="product">
                 <img src="Penna1.jpg">
