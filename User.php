9e4ba2f5c2ae0293faf44bac8bacf96ffdf68fca
OskarHavo <32604330+OskarHavo@users.noreply.github.com>
2019-11-17 01:02:51 +0100

    Påbörjat sidan där man skapar en user

---
+++ User.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+
+    <body >
+
+
+        <?php include("navbar.php"); ?>
+
+            <div class=userdiv>
+            <form >
+                <fieldset>
+                <legend>Skapa en användare!</legend>    
+                    
+                Username:<br>
+                <input type="text" name="username" value="BastionMain">
+                <br>
+                Password: <br>
+                <input type="text" name="password" value="titta inte i adressfält">
+                <br>
+                <input type="submit" value="Submit">
+
+        
+        
+        
+            </fieldset>
+            </form>
+        </div>
+
+
+    </body>
+</html>
