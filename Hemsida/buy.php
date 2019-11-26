8d2e153cee644ba5a0a55222ba2cd6897c6e4461
robdan7 <robdan-7@student.ltu.se>
2019-11-26 16:13:22 +0100

    Nu kan du logga in och lägga saker i kundvagnen.

--- Hemsida/buy.php
+++ Hemsida/buy.php
@@ -1,19 +1,42 @@
 
 <?php
 /* Det här fungerar het enkelt som ett script som lägger till en produkt i varukorgen */
+    include_once($_SERVER['DOCUMENT_ROOT']."/server_connect.php");
     include_once($_SERVER['DOCUMENT_ROOT']."/redirect.php");
     $productID = $_GET["ProductNumber"];
-    //$user = verifySession($_GET["sessionID"]);
+    $user = verifySession($_GET["sessionID"]);
     if (!$user) {
         redirect($_SERVER['HTTP_REFERER']);
     }
     $connection = server_connect();
-    $username = (($connection->query("SELECT CustomerID FROM Sessions WHERE SessionID=".$sessionID))->fetch_assoc())["CustomerID"];
+    $has_shoppingcart = $connection->query("SELECT ShoppingcartID FROM Accounts WHERE CustomerID='".$user."';");
+    $cartID = $has_shoppingcart->fetch_assoc();
+    if ($cartID["ShoppingcartID"] == NULL) {
+        /* Lägg till en ny order och kundvagn*/
+        $connection->query("INSERT INTO OrderNumbers(CustomerID) VALUES('".$user."');");
+        $cartquery = $connection->query("SELECT MAX(OrderID) AS cartID FROM OrderNumbers WHERE CustomerID='".$user."';");
+        $cartID = $cartquery->fetch_assoc();
+        echo $cartID["cartID"];
+        $connection->query("UPDATE Accounts SET ShoppingcartID='".$cartID["cartID"]."' WHERE CustomerID='".$user."';");
+    } else {
+        $cartquery = $connection->query("SELECT ShoppingcartID AS cartID FROM Accounts WHERE CustomerID='".$user."';");
+        $cartID = $cartquery->fetch_assoc();
+    }
 
+    $product_query = $connection->query("SELECT Quantity FROM Orders WHERE OrderID='".$cartID["cartID"]."' AND ProductNumber='".$productID."';");
+
+    if ($product_query->num_rows > 0) {
+        $product = $product_query->fetch_assoc();
+        $connection->query("UPDATE Orders SET Quantity='".($product["Quantity"]+1)."' WHERE OrderID='".$cartID["cartID"]."' AND ProductNumber='".$productID."';");
+    } else {
+        $connection->query("INSERT INTO Orders VALUES('".$cartID["cartID"]."','1','".$productID."','".$user."');");
+        echo "I'm not updating properly";
+    }
+    $connection->close();
 
 
     /* Återgå till produktsidan, fiffigt va?*/
-    //redirect($_SERVER['HTTP_REFERER']);
+    redirect($_SERVER['HTTP_REFERER']);
 ?>
 <!DOCTYPE html>
 <html>
