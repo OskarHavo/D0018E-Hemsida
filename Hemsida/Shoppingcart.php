ebf58aa2261b10e8e711016fefe39d02768e5a39
OskarHavo <32604330+OskarHavo@users.noreply.github.com>
2019-11-28 17:49:47 +0100

    Fixat divarna

--- Hemsida/Shoppingcart.php
+++ Hemsida/Shoppingcart.php
@@ -1,95 +1,52 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT']."/redirect.php");
-include_once($_SERVER['DOCUMENT_ROOT']."/server_connect.php");
+    include_once($_SERVER['DOCUMENT_ROOT']."/server_connect.php");
 
-require_login();
-$conn = server_connect();
-$cart_query = $conn->query("SELECT ShoppingcartID FROM Accounts WHERE CustomerID='".$user."';");
-$cartID = $cart_query->fetch_assoc();
-if ($cartID["ShoppingcartID"] == NULL) {
-    redirect("Shoppingcart_empty.php?sessionID=". $_GET["sessionID"]);
-}
-$conn->close();
-function create_cart() {
-    global $user;
-    global $cartID;
-    global $total_cost;
-    global $cartID;
-    global $can_buy;
-    $can_buy = TRUE;
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $connection = server_connect();
+        $key = validate_user($_POST["username"],$_POST["password"],$connection);
+        $connection->close();
 
-    //$cart_query = $conn->query("SELECT ShoppingcartID FROM Accounts WHERE CustomerID='".$user."';");
-    global $cart_query;
-
-    $conn = server_connect();
-
-
-    $shoppingcart = $conn->query("SELECT Orders.Quantity, Products.ProductName,Products.ProductPrice,Products.InStock FROM Orders INNER JOIN Products ON Products.ProductNumber=Orders.ProductNumber WHERE Orders.CustomerID='".$user."' AND Orders.Price IS NULL AND Orders.OrderID='".$cartID["ShoppingcartID"]."';
-");
-    if ($shoppingcart->num_rows > 0) {
-        while ($product = $shoppingcart->fetch_assoc() ) {
-            if (($product["InStock"] - $product["Quantity"]) < 0) {
-                $can_buy = FALSE;
-            }
-            $product_cost = $product["ProductPrice"]*$product["Quantity"];
-            $total_cost = $total_cost + $product_cost;
-            echo "<tr";
-            if ($product["InStock"] - $product["Quantity"] <= 0) {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                /* Det här indikerar att man vill köpa för många av produkten. Ändra stil om du vill.
-                *  Det vore även bra om du kan lägga till en knapp för att ta bort en vara. Gör en test
-                *  <tr><td>...</td></tr> med knapp där nere; du behöver inte fippla med php:n.
-                */
-
-
-                echo " bgcolor='#FF0000'";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            }
-            echo ">";
-            echo    "<td>".$product["ProductName"]."</td>";
-            echo    "<td>".$product["Quantity"]."</td>";
-            echo    "<td>".$product_cost.":-</td>";
-            echo "</tr>";
+        if (!$key) {
+            redirect("Login.php");
+        } else {
+            redirect("Userpage.php?sessionID=".$key);
+        }
+        $connection->close();
+        $user = $_POST["username"];
+    } else {
+        $session = $_GET["sessionID"];
+        if(!$session) {
+            redirect("Login.php");
+        } else if (!($user = verifySession($session))) {
+            redirect("Login.php");
         }
     }
-    $conn->close();
 
+
+
+
+
+
+function create_cart() {
+    global $user;
+    $conn = server_connect();
+    $cart_query = $conn->query("SELECT ShoppingcartID FROM Accounts WHERE CustomerID='".$user."';");
+    if ($cart_query->num_rows > 0) {
+        $cartID = $cart_query->fetch_assoc();
+
+        $shoppingcart = $conn->query("SELECT Orders.Quantity, Products.ProductName,Products.ProductPrice FROM Orders INNER JOIN Products ON Products.ProductNumber=Orders.ProductNumber WHERE Orders.CustomerID='".$user."' AND Orders.Price IS NULL;
+");
+        if ($shoppingcart->num_rows > 0) {
+            while ($product = $shoppingcart->fetch_assoc() ) {
+                echo "<tr>";
+                echo    "<td>".$product["ProductName"]."</td>";
+                echo    "<td>".$product["Quantity"]."</td>";
+                echo    "<td>".$product["ProductPrice"]."</td>";
+                echo "</tr>";
+            }
+        }
+    }
 }
 
 ?>
@@ -112,45 +69,22 @@ function create_cart() {
             <div id="shoppingcartrubric">
                 <p>Min Shoppingvagn</p>
             </div>
-
+            
+            <div id="shoppingcartdiv">      
             <table class="shoppingcart-table">
                 <tbody>
-                    <tr><td>Produkt</td><td>Antal</td><td>Kostnad</td></tr>
                     <?php
-                        create_cart()
+                    create_cart()
                     ?>
-                    <!-- Här kan du göra en test med delete knapp, tack.-->
-                    <!-- <tr>
-                        <td>
-
-                        </td>
-                    </tr> -->
                 </tbody>
             </table>  
+             </div>
+            
             <div id="shoppingcartcheckout">
-                <p>Total Summa: <?php echo $total_cost;?> :-</p>
+                <p>Total Summa = x kr</p>
             </div>
             <div id="shoppingcartcheckoutbutton">
-                <a <?php
-                   if ($can_buy) {
-                       echo "href='checkout.php";
-                       if ($cartID) {
-
-                           echo '?OrderID='.$cartID["ShoppingcartID"];
-                       }
-                       echo "'";
-                   } else {
-                       echo "href='#'";
-                   }
-
-
-                   ?>><?php
-                    if ($can_buy) {
-                        echo "Köp";
-                    } else {
-                        echo "Slut it lager";
-                    }
-                    ?></a>
+                <p>Slutför köp</p>
             </div>
 
 
--- Hemsida/navbar.php
+++ Hemsida/navbar.php
@@ -27,9 +27,6 @@ function create_category() {
     }
     $connection ->close();
 }
-
-$user = get_session_username();
-
 ?>
 <script>
 
@@ -80,16 +77,17 @@ $user = get_session_username();
 
         </ul>
 
-
-        <?php
-        if ($user) {
-            include_once($_SERVER['DOCUMENT_ROOT']."/userform.php");
-        }
-
-    ?>
+        <button class="openFormButton" id="userformbutton" onclick="openForm()">Du är inloggad som användare XXX</button>
     </div>
 
+    <div class="form-popup" id="userform">
+        <form action="logout.php" class="form-container">
+            <h1>Du är inloggad som XXX</h1>
 
+            <button type="submit" class="knapp">Logga ut!</button>
+            <button type="button" class="knapp avbryt" onclick="closeForm()">Avbryt</button>
+        </form>
+    </div> 
 
 
 
