001f8ec0bb20eb59756e2081b6e1d9bf2cdf4c61
OskarHavo <32604330+OskarHavo@users.noreply.github.com>
2019-11-28 19:18:45 +0100

    Fixat det igen..

--- Hemsida/Shoppingcart.php
+++ Hemsida/Shoppingcart.php
@@ -1,52 +1,52 @@
 <?php
-include_once($_SERVER['DOCUMENT_ROOT']."/redirect.php");
-include_once($_SERVER['DOCUMENT_ROOT']."/server_connect.php");
-require_login();
-$conn = server_connect();
-$cart_query = $conn->query("SELECT ShoppingcartID FROM Accounts WHERE CustomerID='".$user."';");
-$cartID = $cart_query->fetch_assoc();
-if ($cartID["ShoppingcartID"] == NULL) {
-    redirect("Shoppingcart_empty.php?sessionID=". $_GET["sessionID"]);
-}
-$conn->close();
-function create_cart() {
-    global $user;
-    global $cartID;
-    global $total_cost;
-    global $cartID;
-    global $can_buy;
-    $can_buy = TRUE;
-    //$cart_query = $conn->query("SELECT ShoppingcartID FROM Accounts WHERE CustomerID='".$user."';");
-    global $cart_query;
-    $conn = server_connect();
-    $shoppingcart = $conn->query("SELECT Orders.Quantity, Products.ProductName,Products.ProductPrice,Products.InStock FROM Orders INNER JOIN Products ON Products.ProductNumber=Orders.ProductNumber WHERE Orders.CustomerID='".$user."' AND Orders.Price IS NULL AND Orders.OrderID='".$cartID["ShoppingcartID"]."';
-");
-    if ($shoppingcart->num_rows > 0) {
-        while ($product = $shoppingcart->fetch_assoc() ) {
-            if (($product["InStock"] - $product["Quantity"]) < 0) {
-                $can_buy = FALSE;
-            }
-            $product_cost = $product["ProductPrice"]*$product["Quantity"];
-            $total_cost = $total_cost + $product_cost;
-            echo "<tr";
-            if ($product["InStock"] - $product["Quantity"] <= 0) {
-                /* Det här indikerar att man vill köpa för många av produkten. Ändra stil om du vill.
-                *  Det vore även bra om du kan lägga till en knapp för att ta bort en vara. Gör en test
-                *  <tr><td>...</td></tr> med knapp där nere; du behöver inte fippla med php:n.
-                */
-                echo " bgcolor='#FF0000'";
-            }
-            echo ">";
-            echo    "<td>".$product["ProductName"]."</td>";
-            echo    "<td>".$product["Quantity"]."</td>";
-            echo    "<td>".$product_cost.":-</td>";
-            echo "</tr>";
-        }
-    }
-    $conn->close();
-}
-?>
-
+	include_once($_SERVER['DOCUMENT_ROOT']."/redirect.php");
+	include_once($_SERVER['DOCUMENT_ROOT']."/server_connect.php");
+	require_login();
+	$conn = server_connect();
+	$cart_query = $conn->query("SELECT ShoppingcartID FROM Accounts WHERE CustomerID='".$user."';");
+	$cartID = $cart_query->fetch_assoc();
+	if ($cartID["ShoppingcartID"] == NULL) {
+	    redirect("Shoppingcart_empty.php?sessionID=". $_GET["sessionID"]);
+	}
+	$conn->close();
+	function create_cart() {
+	    global $user;
+	    global $cartID;
+	    global $total_cost;
+	    global $cartID;
+	    global $can_buy;
+	    $can_buy = TRUE;
+	    //$cart_query = $conn->query("SELECT ShoppingcartID FROM Accounts WHERE CustomerID='".$user."';");
+	    global $cart_query;
+	    $conn = server_connect();
+	    $shoppingcart = $conn->query("SELECT Orders.Quantity, Products.ProductName,Products.ProductPrice,Products.InStock FROM Orders INNER JOIN Products ON Products.ProductNumber=Orders.ProductNumber WHERE Orders.CustomerID='".$user."' AND Orders.Price IS NULL AND Orders.OrderID='".$cartID["ShoppingcartID"]."';
+	");
+	    if ($shoppingcart->num_rows > 0) {
+	        while ($product = $shoppingcart->fetch_assoc() ) {
+	            if (($product["InStock"] - $product["Quantity"]) < 0) {
+	                $can_buy = FALSE;
+	            }
+	            $product_cost = $product["ProductPrice"]*$product["Quantity"];
+	            $total_cost = $total_cost + $product_cost;
+	            echo "<tr";
+	            if ($product["InStock"] - $product["Quantity"] <= 0) {
+	                /* Det här indikerar att man vill köpa för många av produkten. Ändra stil om du vill.
+	                *  Det vore även bra om du kan lägga till en knapp för att ta bort en vara. Gör en test
+	                *  <tr><td>...</td></tr> med knapp där nere; du behöver inte fippla med php:n.
+	                */
+	                echo " bgcolor='#FF0000'";
+	            }
+	            echo ">";
+	            echo    "<td>".$product["ProductName"]."</td>";
+	            echo    "<td>".$product["Quantity"]."</td>";
+	            echo    "<td>".$product_cost.":-</td>";
+	            echo "</tr>";
+	        }
+	    }
+	    $conn->close();
+	}
+	?>
+	
 
 
 
@@ -65,41 +65,23 @@ function create_cart() {
             <div id="shoppingcartrubric">
                 <p>Min Shoppingvagn</p>
             </div>
-
+            
+            <div id="shoppingcartdiv">      
             <table class="shoppingcart-table">
                 <tbody>
                     <tr><td>Produkt</td><td>Antal</td><td>Kostnad</td></tr>
                     <?php
-                        create_cart()
+                    create_cart()
                     ?>
-                    <!-- Här kan du göra en test med delete knapp, tack.-->
-                    <!-- <tr>
-                        <td>
-                        </td>
-                    </tr> -->
                 </tbody>
-            </table>
+            </table>  
+             </div>
+            
             <div id="shoppingcartcheckout">
-                <p>Total Summa: <?php echo $total_cost;?> :-</p>
+                <p>Total Summa = x kr</p>
             </div>
             <div id="shoppingcartcheckoutbutton">
-                <a <?php
-                   if ($can_buy) {
-                       echo "href='checkout.php";
-                       if ($cartID) {
-                           echo '?OrderID='.$cartID["ShoppingcartID"];
-                       }
-                       echo "'";
-                   } else {
-                       echo "href='#'";
-                   }
-                   ?>><?php
-                    if ($can_buy) {
-                        echo "Köp";
-                    } else {
-                        echo "Slut it lager";
-                    }
-                    ?></a>
+                <p>Slutför köp</p>
             </div>
 
 
--- Hemsida/Userpage.php
+++ Hemsida/Userpage.php
@@ -1,89 +1,89 @@
 <?php
-    include_once($_SERVER['DOCUMENT_ROOT']."/redirect.php");
-    include_once($_SERVER['DOCUMENT_ROOT']."/server_connect.php");
-
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $connection = server_connect();
-        $key = validate_user($_POST["username"],$_POST["password"],$connection);
-        $connection->close();
-
-        if (!$key) {
-            redirect("Login.php");
-        } else {
-            redirect("Userpage.php?sessionID=".$key);
-        }
-        $connection->close();
-        $user = $_POST["username"];
-    } else {
-        $session = $_GET["sessionID"];
-        if(!$session) {
-            redirect("Login.php");
-        } else if (!($user = verifySession($session))) {
-            redirect("Login.php");
-        }
-    }
-
-function create_orders() {
-    global $user;
-    $connection = server_connect();
-    $query = $connection->query("SELECT OrderID, Quantity, ProductNumber, Price FROM Orders WHERE CustomerID='robin' AND Price IS NOT NULL;");
-    if ($query->num_rows > 0) {
-        while ($row = $query->fetch_assoc()) {
-            /* Det här är bara en massa <tr> med <td> inuti. */
-            echo "<tr>";
-            echo    "<td>".$row["OrderID"]."</td>";
-            echo    "<td>".$row["ProductNumber"]."</td>";
-            echo    "<td>".$row["Quantity"]."</td>";
-            echo    "<td>".$row["Price"].":-</td>";
-            echo "</tr>";
-        }
-    }
-
-    $connection->close();
-}
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="style.css">
-        <script src="javascripts.js"></script>
-    </head>
-
-    <body onload="setSessionID('<?php echo fetchSessionID();?>')">
-
-
-        <?php include("navbar.php"); ?>
-
-          <div id="container">
-
-            <div id="userpagerubric">
-                <p>Mina Ordrar(<?php echo $user;?>)</p>
-            </div>
-
-            <div id="orderdiv">
-            <table class="userpage-table">
-                <tbody>
-                    <tr>
-                        <th scope="row">Ordernummer</th>
-                        <td>Produktnummer<td>
-                        <td>antal</td>
-                        <td>Pris</td>
-                    </tr>
-                    <?php
-                        create_orders();
-                    ?>
-                </tbody>
-
-            </table>
-            </div>
-            <div id="logoutdiv">
-                <p><a href="logout.php">Logga ut</a></p>
-            </div>
-
-
-        </div>
-
-
-    </body>
-</html>
+	    include_once($_SERVER['DOCUMENT_ROOT']."/redirect.php");
+	    include_once($_SERVER['DOCUMENT_ROOT']."/server_connect.php");
+	
+	    if($_SERVER["REQUEST_METHOD"] == "POST"){
+	        $connection = server_connect();
+	        $key = validate_user($_POST["username"],$_POST["password"],$connection);
+	        $connection->close();
+	
+	        if (!$key) {
+	            redirect("Login.php");
+	        } else {
+	            redirect("Userpage.php?sessionID=".$key);
+	        }
+	        $connection->close();
+	        $user = $_POST["username"];
+	    } else {
+	        $session = $_GET["sessionID"];
+	        if(!$session) {
+	            redirect("Login.php");
+	        } else if (!($user = verifySession($session))) {
+	            redirect("Login.php");
+	        }
+	    }
+	
+	function create_orders() {
+	    global $user;
+	    $connection = server_connect();
+	    $query = $connection->query("SELECT OrderID, Quantity, ProductNumber, Price FROM Orders WHERE CustomerID='robin' AND Price IS NOT NULL;");
+	    if ($query->num_rows > 0) {
+	        while ($row = $query->fetch_assoc()) {
+	            /* Det här är bara en massa <tr> med <td> inuti. */
+	            echo "<tr>";
+	            echo    "<td>".$row["OrderID"]."</td>";
+	            echo    "<td>".$row["ProductNumber"]."</td>";
+	            echo    "<td>".$row["Quantity"]."</td>";
+	            echo    "<td>".$row["Price"].":-</td>";
+	            echo "</tr>";
+	        }
+	    }
+	
+	    $connection->close();
+	}
+	?>
+	<!DOCTYPE html>
+	<html>
+	    <head>
+	        <meta charset="utf-8">
+	        <link rel="stylesheet" href="style.css">
+	        <script src="javascripts.js"></script>
+	    </head>
+	
+	    <body onload="setSessionID('<?php echo fetchSessionID();?>')">
+	
+	
+	        <?php include("navbar.php"); ?>
+	
+	          <div id="container">
+	
+	            <div id="userpagerubric">
+	                <p>Mina Ordrar (<?php echo $user;?>)</p>
+	            </div>
+	
+	            <div id="orderdiv">
+	            <table class="userpage-table">
+	                <tbody>
+	                    <tr>
+	                        <th scope="row">Ordernummer</th>
+	                        <td>Produktnummer<td>
+	                        <td>antal</td>
+	                        <td>Pris</td>
+	                    </tr>
+	                    <?php
+	                        create_orders();
+	                    ?>
+	                </tbody>
+	
+	            </table>
+	            </div>
+	            <div id="logoutdiv">
+	                <p><a href="logout.php">Logga ut</a></p>
+	            </div>
+	
+	
+	        </div>
+	
+	
+	    </body>
+	</html>
